67fb36034a3ebcbb6eeb096424eebfa01220d641
Denny <cubeintocube@gmail.com>
2016-05-23 21:59:24 +0500

    test

--- src/main/webapp/app/dashboard.component.ts
+++ src/main/webapp/app/dashboard.component.ts
@@ -21,6 +21,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     //this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1,5));
       this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    //
   }
 
   gotoDetail(hero: Hero) {
